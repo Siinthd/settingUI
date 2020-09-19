cb101ecb3d323dfda7d82b6d9d1c14b5a1704bf0
Siinthd <Siinthd@protonmail.com>
2020-09-19 21:00:18 +0300

    add second form

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
+#include <QGuiApplication>
 #include <QApplication>
+#include <QQmlApplicationEngine>
 #include "settingwindow.h"
 
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
 
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+    QApplication a(argc, argv);
     SettingWindow c;
 
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
     c.show();
-    return a.exec();
+    //return a.exec();
+    return app.exec();
 }
